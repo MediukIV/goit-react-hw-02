a2f05501b13a771ca9354e393cae86cd022555ba
MediukIV <143748068+MediukIV@users.noreply.github.com>
2024-03-14 22:03:34 +0200

    add_finish

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -3,26 +3,12 @@ import css from './Options.module.css'
 const Options = ({ updateFeedback, totalFeedback }) => {
   return (
  <div className={css.options}>
-      <button className={css.optionsBtn} onClick={() => updateFeedback("good")}>
-        Good
-      </button>
-      <button
-        className={css.optionsBtn}
-        onClick={() => updateFeedback("neutral")}
-      >
-        Neutral
-      </button>
-      <button className={css.optionsBtn} onClick={() => updateFeedback("bad")}>
-        Bad
-      </button>
-      {totalFeedback > 0 && (
-        <button
-          className={css.optionsBtn}
-          onClick={() => updateFeedback("reset")}
-        >
-          Reset
-        </button>
-      )}
+      <button className={css.optionsBtn} onClick={() => updateFeedback("good")}>Good</button>
+      <button className={css.optionsBtn} onClick={() => updateFeedback("neutral")}>Neutral</button>
+      <button className={css.optionsBtn} onClick={() => updateFeedback("bad")}>Bad</button>
+      {totalFeedback > 0 &&
+        (<button className={css.optionsBtn} onClick={() => updateFeedback("reset")}>Reset</button>)
+      }
     </div>
   )
 }
